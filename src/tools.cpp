d46a64731e353320ee44f3bbb8beb392c68b4550
anjaseverin <anjaseverin19@gmail.com>
2018-01-03 16:54:59 +0100

    c

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,85 +11,32 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-    /*
     VectorXd rmse(4);
-        rmse << 0,0,0,0;
+    rmse << 0,0,0,0;
 
-        // check the validity of the following inputs:
-        //  * the estimation vector size should not be zero
-        //  * the estimation vector size should equal ground truth vector size
-        if(estimations.size() != ground_truth.size()
-                || estimations.size() == 0){
-            cout << "Invalid estimation or ground_truth data" << endl;
-            return rmse;
-        }
-
-        //accumulate squared residuals
-        for(unsigned int i=0; i < estimations.size(); ++i){
-
-            VectorXd residual = estimations[i] - ground_truth[i];
-
-            //coefficient-wise multiplication
-            residual = residual.array()*residual.array();
-            rmse += residual;
-        }
-
-        //calculate the mean
-        rmse = rmse/estimations.size();
-
-        //calculate the squared root
-        rmse = rmse.array().sqrt();
-
-        //return the result
+    // check the validity of the following inputs:
+    //  * the estimation vector size should not be zero
+    //  * the estimation vector size should equal ground truth vector size
+    if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+        cout << "Invalid estimation or ground_truth data" << endl;
         return rmse;
-        */
-    float t = estimations.size(); // Current timestep index
+    }
 
-    // check the validity of the inputs
-    if( t == 0 )
-      cout << "Error in CalculateRMSE:  estimations.size() = 0" << endl;
-    if( t != ground_truth.size() )
-      cout << "Error in CalculateRMSE: sizes of estimation and ground truth do not match" << endl;
+    //accumulate squared residuals
+    for(unsigned int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
 
-    // Rather than recomputing from the entire estimations array,
-    // I store the running error from the last timestep persistently.
-    // This ensures that CalculateRMSE() remains O(1) instead of O(N)
-    // at the Nth timestep.
-    //
-    // for(int i=0; i < estimations.size(); i++)
-    // {
+        //coefficient-wise multiplication
+        residual = residual.array()*residual.array();
+        rmse += residual;
+    }
 
-    // Recover the running sum of the error, rather than the running
-    // mean, so we can add the current residual
-    mse = mse*(t-1);
+    //calculate the mean
+    rmse = rmse/estimations.size();
 
-    // Add the current residual using a Kahan sum to minimize floating-point
-    // rounding error.
-    residual = estimations[t-1] - ground_truth[t-1];
-    residual = residual.array()*residual.array();
-    residual += kahanerror;
-    mse += residual;
-    kahanerror = residual - ( mse - lastmse );
-    lastmse = mse;
+    //calculate the squared root
+    rmse = rmse.array().sqrt();
 
-    // }
-
-    // Calculate the new mean
-    mse = mse/estimations.size();
-
-    // Calculate the RMSE
-    rmse = mse.array().sqrt();
-
-    // cout << estimations.size() << endl;
-    if( rmse(0) > .09 ||
-        rmse(1) > .10 ||
-        rmse(2) > .40 ||
-        rmse(3) > .30 )
-      cout << "Warning at timestep " << t << ":  rmse = "
-           << rmse(0) << "  " << rmse(1) << "  "
-           << rmse(2) << "  " << rmse(3) << endl
-           << " currently exceeds tolerances of "
-           << ".09, .10, .40, .30" << endl;
-
+    //return the result
     return rmse;
 }
--- src/ukf.cpp
+++ src/ukf.cpp
@@ -29,7 +29,7 @@ UKF::UKF() {
   std_a_ = 3;//5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ =1;// 2.4;
+  std_yawdd_ =0.5;// 2.4;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
