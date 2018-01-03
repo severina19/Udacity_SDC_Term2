265a9811e496becadb8eeb09c3d301377f146c79
anjaseverin <anjaseverin19@gmail.com>
2018-01-03 16:32:47 +0100

    tune

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -26,10 +26,10 @@ UKF::UKF() {
   // initial covariance matrix
   P_ = MatrixXd(5, 5);
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 5;
+  std_a_ = 3;//5;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 2.4;
+  std_yawdd_ =1;// 2.4;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
